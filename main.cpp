dc355cef05b82cffb5a0c7c09d13250df51b4b16
Daniel Ogbuigwe <103327095+ogbidaniel@users.noreply.github.com>
2024-10-04 21:38:05 +0100

    add initialize the main.cpp of the commandlinecalc

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+double add(){
+
+}
+double subtract(){
+    
+}
+double multiply(){
+    
+}
+double divide(){
+    
+}
+
+int main(){
+    std::cout << "___________________Commandline Calculator App!___________________";
+    std::cout << "Enter your number and operations. Enter the equals sign to print the results.\n";
+    unsigned int x{};
+    std::cout << &x;
+    return 0;
+}
