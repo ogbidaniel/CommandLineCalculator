e7fc36b2a71a0d64a9533f1b7e5aca3791a83e63
Daniel Ogbuigwe <103327095+ogbidaniel@users.noreply.github.com>
2024-10-05 20:33:09 +0100

    commandline calculator

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ double divide(){
 }
 
 int main(){
-    std::cout << "___________________Commandline Calculator App!___________________";
+    std::cout << "___________________Command line Calculator App!___________________";
     std::cout << "Enter your number and operations. Enter the equals sign to print the results.\n";
     unsigned int x{};
     std::cout << &x;
