11f6bb10b0be1942248e8fb0c5dc2932fa340e34
Daniel Ogbuigwe <103327095+ogbidaniel@users.noreply.github.com>
2024-10-05 20:33:49 +0100

    fleshed out main.cpp for the caculator

--- main.cpp
+++ main.cpp
@@ -1,23 +1,63 @@
 #include <iostream>
 #include <cmath>
 
-double add(){
-
+double getNumber(){
+    double number;
+    std::cout << "Enter number: ";
+    std::cin >> number;
+    return number;
 }
-double subtract(){
-    
+char getOperation(){
+    char operation;
+    std::cout << "Enter operation: ";
+    std::cin >> operation;
+    return operation;
 }
-double multiply(){
-    
-}
-double divide(){
-    
+double operate(double num1, double num2, char operation){
+    //operates on the values based on the current operation 
+    //entered and the numbers in consideration
+    switch(operation){
+        case '+':
+            return num1 + num2;
+            break;
+        case '-':
+            return num1 - num2;
+            break;
+        case '*':
+            return num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0){
+                return num1 / num2;
+            } else{
+                std::cerr << "Division by zero error!" << std::endl;
+                return 0;
+            }
+            break;
+        default:
+            std::cerr << "Invalid operator!";
+            return 0;
+    }
 }
 
 int main(){
-    std::cout << "___________________Command line Calculator App!___________________";
-    std::cout << "Enter your number and operations. Enter the equals sign to print the results.\n";
-    unsigned int x{};
-    std::cout << &x;
+    std::cout << "___________________Command line Calculator App!___________________\n";
+    std::cout << "Enter your numbers and operations. Enter the '=' for the results.\n";
+    char operation;
+    double num1{0};
+    double num2{0};
+    double output{num1};
+
+    while(operation != '='){
+        num1 = getNumber();
+        output = num1; // assign output to the first enterd number
+        operation = getOperation();
+        std::cout << "output: " << output << " operation: " << operation << std::endl; // for debuging
+        num2 = getNumber();
+        output = operate(num1, num2, operation);
+        operation = getOperation();
+        std::cout << "output: " << output << " operation: " << operation << std::endl; // for debuging
+    };
+    std::cout << "Answer: " << output << std::endl;
     return 0;
 }
