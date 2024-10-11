7ce95dc5ed38a2b701cb7207cb68c93aee7b3f92
Daniel Ogbuigwe <103327095+ogbidaniel@users.noreply.github.com>
2024-10-11 23:16:06 +0100

    initialize dig

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 
 void tokenizer(const string& problem){
+    char dig[11] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'};
     for(size_t i = 0; i < problem.size(); i++){
         char currentChar = problem[i];
 
