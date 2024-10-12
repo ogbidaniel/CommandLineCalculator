ef0437a3e31b27a92e611ed1298113ee4fc7b120
Daniel Ogbuigwe <103327095+ogbidaniel@users.noreply.github.com>
2024-10-12 12:38:19 +0100

    updated tokenizer

--- main.cpp
+++ main.cpp
@@ -5,38 +5,43 @@ using namespace std;
 
 void tokenizer(const string& problem){
     char dig[11] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'};
+    char opp[4] = {'+', '-', '*', '/'};
+    char par[2] = {'(', ')'};
+    string number = "";
+    bool decimalAdded{false};
+
+
     for(size_t i = 0; i < problem.size(); i++){
         char currentChar = problem[i];
 
         // check for digit
         if (isdigit(currentChar)){
-            cout << "digit: " << currentChar << "\n";
+            number = number + currentChar;
         }
-        // check for operator
-        else if (currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/'){
-            cout << "operator: " << currentChar << "\n";
-        }
-        // check for parentheses
-        else if (currentChar == '(' || currentChar == ')'){
-            cout << "parenthesis: " << currentChar << "\n";
-        }
-        // ignore whitespaces
-        else if (isspace(currentChar)){
-            continue;
-        }
-
-        // check for decimal
+        // check decimal
         else if (currentChar == '.'){
-            cout << "decimal: " << currentChar << "\n";
-        }   
-
-        // unknown char
-        else{
-            cout << "unknown\n";
+            if (decimalAdded == false){
+                number = number + currentChar;
+                decimalAdded = true;
+            }
+            else{
+                cout << "Error in input problem\n";
+                return;
+            }
         }
-        
-        
-
+        // end accumulator and print out other parts of the problem
+        else{
+            if (number != ""){
+                cout << number << endl;
+                number = "";
+                decimalAdded = false;
+            }
+            cout << currentChar << endl;
+        }
+    }
+    // print the final token
+    if (number != ""){
+        cout << number << endl;
     }
 }
 
